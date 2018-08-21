5f73b3fa06a6a19fabf8a63db23ad0460d488698
Angel Pino <angelmauricio.pinogonzalez@gmail.com>
2018-08-21 14:34:32 -0300

    Update regiones.rb

--- regiones.rb
+++ regiones.rb
@@ -1,22 +1,22 @@
-def migrate(dir)
-  super
-  if dir == :up
-    execute "INSERT INTO `regiones` (`nombre`,`titulo`) VALUES 
-     ('I','TARAPACA'),
-     ('II','ANTOFAGASTA'),
-     ('III','ATACAMA'),
-     ('IV','COQUIMBO'),
-     ('V','VALPARAISO'),
-     ('VI',\"LIBERTADOR GENERAL BERNARDO O\'HIGGINS\"),
-     ('VII','MAULE'),
-     ('VIII','BIO-BIO'),
-     ('IX','LA ARAUCANIA'),
-     ('X','LOS LAGOS'),
-     ('XI','AYSEN DEL GENERAL CARLOS IBA\u00d1EZ DEL CAMPO'),
-     ('XII','MAGALLANES Y LA ANTARTICA CHILENA'),
-     ('XIII','METROPOLITANA'),
-     ('XIV','LOS RIOS'),
-     ('XV','ARICA Y PARINACOTA');"
+  def migrate(dir)
+    super
+    if dir == :up
+      execute "INSERT INTO `regiones` (`nombre`,`titulo`) VALUES 
+       ('I','TARAPACA'),
+       ('II','ANTOFAGASTA'),
+       ('III','ATACAMA'),
+       ('IV','COQUIMBO'),
+       ('V','VALPARAISO'),
+       ('VI',\"LIBERTADOR GENERAL BERNARDO O\'HIGGINS\"),
+       ('VII','MAULE'),
+       ('VIII','BIO-BIO'),
+       ('IX','LA ARAUCANIA'),
+       ('X','LOS LAGOS'),
+       ('XI','AYSEN DEL GENERAL CARLOS IBA\u00d1EZ DEL CAMPO'),
+       ('XII','MAGALLANES Y LA ANTARTICA CHILENA'),
+       ('XIII','METROPOLITANA'),
+       ('XIV','LOS RIOS'),
+       ('XV','ARICA Y PARINACOTA');"
+    end
   end
-end
 
