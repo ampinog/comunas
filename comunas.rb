b08ca4957f4ced4cd5586c8bb8c99b9d665f1f65
Angel Pino <angelmauricio.pinogonzalez@gmail.com>
2018-08-21 13:06:59 -0300

    Migrate regiones, provincias y comunas
    Estos cargan regiones, los crea directamente, provincias y comunas lo hace desde archivos csv que deben estar en directorio db.

---
+++ comunas.rb
@@ -0,0 +1,11 @@
+def migrate( dir)
+  super
+  if dir == :up
+    execute "LOAD DATA LOCAL INFILE 'db/comunas.csv'
+    INTO TABLE comunas
+    FIELDS TERMINATED BY ';'
+    LINES TERMINATED BY '\n'
+    (id, nombre, provincia_id);"
+  end
+end
+
---
+++ provincias.rb
@@ -0,0 +1,11 @@
+def migrate( dir)
+  super
+  if dir == :up
+    execute "LOAD DATA LOCAL INFILE 'db/provincias.csv'
+    INTO TABLE provincias
+    FIELDS TERMINATED BY ';'
+    LINES TERMINATED BY '\n'
+    (id, nombre, region_id);"
+  end
+end
+
