dd85fe576d29cd7837797b8555e6ddb943d37b53
Angel Pino <angelmauricio.pinogonzalez@gmail.com>
2018-08-21 15:03:44 -0300

    Elimine fin de linea

--- comunas.rb
+++ comunas.rb
@@ -4,7 +4,6 @@
       execute "LOAD DATA LOCAL INFILE 'db/comunas.csv'
         INTO TABLE comunas
         FIELDS TERMINATED BY ';'
-        LINES TERMINATED BY '\n'
         (id, nombre, provincia_id)
         SET created_at=NOW(), updated_at=NOW();"
     end
