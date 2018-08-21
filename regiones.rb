7659f1100e0644a4bce52fe9ea1b480143d77078
Angel Pino <angelmauricio.pinogonzalez@gmail.com>
2018-08-21 14:42:46 -0300

    Update regiones.rb

--- regiones.rb
+++ regiones.rb
@@ -1,22 +1,22 @@
   def migrate(dir)
     super
     if dir == :up
-      execute "INSERT INTO `regiones` (`nombre`,`titulo`) VALUES 
-       ('I','TARAPACA'),
-       ('II','ANTOFAGASTA'),
-       ('III','ATACAMA'),
-       ('IV','COQUIMBO'),
-       ('V','VALPARAISO'),
-       ('VI',\"LIBERTADOR GENERAL BERNARDO O\'HIGGINS\"),
-       ('VII','MAULE'),
-       ('VIII','BIO-BIO'),
-       ('IX','LA ARAUCANIA'),
-       ('X','LOS LAGOS'),
-       ('XI','AYSEN DEL GENERAL CARLOS IBA\u00d1EZ DEL CAMPO'),
-       ('XII','MAGALLANES Y LA ANTARTICA CHILENA'),
-       ('XIII','METROPOLITANA'),
-       ('XIV','LOS RIOS'),
-       ('XV','ARICA Y PARINACOTA');"
+      execute "INSERT INTO `regiones` (`nombre`,`titulo`, created_at, updated_at) VALUES 
+       ('I','TARAPACA', NOW(), NOW()),
+       ('II','ANTOFAGASTA', NOW(), NOW()),
+       ('III','ATACAMA', NOW(), NOW()),
+       ('IV','COQUIMBO', NOW(), NOW()),
+       ('V','VALPARAISO', NOW(), NOW()),
+       ('VI',\"LIBERTADOR GENERAL BERNARDO O\'HIGGINS\", NOW(), NOW()),
+       ('VII','MAULE', NOW(), NOW()),
+       ('VIII','BIO-BIO', NOW(), NOW()),
+       ('IX','LA ARAUCANIA', NOW(), NOW()),
+       ('X','LOS LAGOS', NOW(), NOW()),
+       ('XI','AYSEN DEL GENERAL CARLOS IBA\u00d1EZ DEL CAMPO',NOW(), NOW()),
+       ('XII','MAGALLANES Y LA ANTARTICA CHILENA', NOW(), NOW()),
+       ('XIII','METROPOLITANA', NOW(), NOW()),
+       ('XIV','LOS RIOS', NOW(), NOW()),
+       ('XV','ARICA Y PARINACOTA', NOW(), NOW());"
     end
   end
 
