02da67113485ec7223df46fb6634e1e0720f5350
Angel Pino <angelmauricio.pinogonzalez@gmail.com>
2018-08-21 14:36:20 -0300

    Update comunas.rb

--- comunas.rb
+++ comunas.rb
@@ -1,11 +1,10 @@
-def migrate( dir)
-  super
-  if dir == :up
-    execute "LOAD DATA LOCAL INFILE 'db/comunas.csv'
-    INTO TABLE comunas
-    FIELDS TERMINATED BY ';'
-    LINES TERMINATED BY '\n'
-    (id, nombre, provincia_id);"
+  def migrate( dir)
+    super
+    if dir == :up
+      execute "LOAD DATA LOCAL INFILE 'db/comunas.csv'
+        INTO TABLE comunas
+        FIELDS TERMINATED BY ';'
+        LINES TERMINATED BY '\n'
+        (id, nombre, provincia_id);"
+    end
   end
-end
-
